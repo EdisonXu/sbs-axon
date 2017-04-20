c2285b85273fc74ca3f64cba3cafce3d7996d215
Edison Xu <edisonxu@outlook.com>
2017-04-20 17:55:53 +0800

    update lesson-7

--- lesson-7/order-service/src/main/java/com/edi/learn/cloud/command/config/CloudConfiguration.java
+++ lesson-7/order-service/src/main/java/com/edi/learn/cloud/command/config/CloudConfiguration.java
@@ -1,11 +1,7 @@
 package com.edi.learn.cloud.command.config;
 
-import org.axonframework.commandhandling.distributed.CommandBusConnector;
-import org.axonframework.commandhandling.distributed.CommandRouter;
-import org.axonframework.commandhandling.distributed.DistributedCommandBus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -17,11 +13,5 @@ public class CloudConfiguration {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
-    }
-
-    @Bean
-    @Primary
-    public DistributedCommandBus distributedCommandBus(CommandRouter router, CommandBusConnector connector){
-        return new DistributedCommandBus(router, connector);
     }
 }
